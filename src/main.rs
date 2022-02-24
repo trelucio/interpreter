2781100cf2fff37a39f0740d17742132506e389b
Therese Relucio <trelucio@udallas.edu>
2022-02-23 18:10:43 -0600

    HW 8: add new main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+/*
+Therese Relucio
+HW 8: February 24, 2022
+
+1. Fork the repo for our interpreter to your own GitHub account, 
+    and then clone that to your local system 
+    - https://github.com/rmirabelli/interpreter
+2. Write a new main() function that is a new expression, 
+    and run the interpreter to watch the result.
+3. Submit your main.rs function via Brightspace.
+4. Provide the URL for your own fork of the interpreter
+    - https://github.com/trelucio/interpreter
+*/
+
 enum Primitive {
     Add,
     Multiply,
@@ -26,4 +40,14 @@ fn main() {
     primitives.push(Primitive::Number(4));
     let result = evaluate(primitives);
     println!("{}", result);
+
+    // New main() function that is a new expression
+    // new vector because primitives vector is out of scope
+    let mut primitives_two =  Vec::<Primitive>::new();
+    primitives_two.push(Primitive::Add);
+    primitives_two.push(Primitive::Number(20));
+    primitives_two.push(Primitive::Number(result));
+     // new_result = 20 + "result" from OG code
+    let new_result = evaluate(primitives_two);
+    println!("New Result: {}", new_result);
 }
