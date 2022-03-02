bfe56c18c5011fef1dce8595c2a36892dfa6c29b
Therese Relucio <trelucio@udallas.edu>
2022-03-02 12:13:39 -0600

    modify for hw 9

--- src/HW 8 main.rs
+++ src/HW 8 main.rs
@@ -1,14 +1,19 @@
 /*
 Therese Relucio
-HW 9: March 3, 2022
+HW 8: February 24, 2022
 
-1. Take the interpreter we have so far (from GitHub), 
-    and add a "subtract" primitive to it.
+1. Fork the repo for our interpreter to your own GitHub account, 
+    and then clone that to your local system 
+    - https://github.com/rmirabelli/interpreter
+2. Write a new main() function that is a new expression, 
+    and run the interpreter to watch the result.
+3. Submit your main.rs function via Brightspace.
+4. Provide the URL for your own fork of the interpreter
+    - https://github.com/trelucio/interpreter
 */
 
 enum Primitive {
     Add,
-    Subtract,
     Multiply,
     Number(i32)
 }
@@ -23,14 +28,12 @@ fn eval_prim(primitive: &Primitive) -> i32 {
 fn evaluate(array: Vec<Primitive>) -> i32 {
     match array[0] {
         Primitive::Add => { eval_prim(&array[1]) + eval_prim(&array[2])}
-        Primitive::Subtract => { eval_prim(&array[1]) - eval_prim(&array[2])}
         Primitive::Multiply => { eval_prim(&array[1]) * eval_prim(&array[2])}
         _ => 0
     }
 }
 
 fn main() {
-    // from original interpreter
     let mut primitives = Vec::<Primitive>::new();
     primitives.push(Primitive::Multiply);
     primitives.push(Primitive::Number(3));
@@ -38,22 +41,13 @@ fn main() {
     let result = evaluate(primitives);
     println!("{}", result);
 
-    // HW 8: New main() function that is a new expression
+    // New main() function that is a new expression
     // new vector because primitives vector is out of scope
     let mut primitives_two =  Vec::<Primitive>::new();
     primitives_two.push(Primitive::Add);
     primitives_two.push(Primitive::Number(20));
     primitives_two.push(Primitive::Number(result));
+     // new_result = 20 + "result" from OG code
     let new_result = evaluate(primitives_two);
     println!("New Result: {}", new_result);
-    
-    // HW 9: Subtraction (25-20=5)
-    let x = 25;
-    let y = 20;
-    let mut sub_primitives =  Vec::<Primitive>::new();
-    sub_primitives.push(Primitive::Subtract);
-    sub_primitives.push(Primitive::Number(x));
-    sub_primitives.push(Primitive::Number(y));
-    let difference = evaluate(sub_primitives);
-    println!("Difference of {} and {} = {}", x, y, difference);
 }
