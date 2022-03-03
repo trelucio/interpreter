0d5334907c6c7afea161fde9b538a8d45826c14c
Therese Relucio <trelucio@udallas.edu>
2022-03-02 23:44:52 -0600

    correct hw 9

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,45 @@
 /*
 Therese Relucio
-HW 9: March 3, 2022
+HW 9: March 2, 2022
 
-1. Take the interpreter we have so far (from GitHub), 
-    and add a "subtract" primitive to it.
+1. Please BAIL OUT on this homework if you have too much trouble with it!
+2. Take the interpreter we have so far (from GitHub), and add a "subtract" primitive to it.
+3. You aren’t going to be able to just use a different rust operator inside a copy of the "Add" arm, 
+    so spend some time thinking about this one
+4. If you legit can’t do this in 20 or 30 minutes, submit your , 
+    and I’ll review those and see how to make it all work out during the next class!
 */
-
+#[derive(Copy, Clone)]
 enum Primitive {
     Add,
-    Subtract,
     Multiply,
-    Number(i32)
-}
-
-fn eval_prim(primitive: &Primitive) -> i32 {
-    match primitive {
-        Primitive::Number(val) => *val,
-        _ => 0
-    }
+    Subtract,
+    Number(i32),
 }
 
 fn evaluate(array: Vec<Primitive>) -> i32 {
-    match array[0] {
-        Primitive::Add => { eval_prim(&array[1]) + eval_prim(&array[2])}
-        Primitive::Subtract => { eval_prim(&array[1]) - eval_prim(&array[2])}
-        Primitive::Multiply => { eval_prim(&array[1]) * eval_prim(&array[2])}
-        _ => 0
+    let element = &array[0];
+    let mut iter = array.iter();
+    iter.next();
+
+    match element {
+        Primitive::Add => iter.fold(0, |total, next| 
+            total + evaluate(vec![*next])),
+        Primitive::Multiply => iter.fold(1, |total, next| 
+            total * evaluate(vec![*next])),
+        Primitive::Number(val) => *val,
+        Primitive::Subtract => {
+            iter.next();
+            iter.fold(evaluate(vec![array[1]]), |total, next| total - evaluate(vec![*next]))},
     }
 }
 
 fn main() {
-    // from original interpreter
-    let mut primitives = Vec::<Primitive>::new();
-    primitives.push(Primitive::Multiply);
-    primitives.push(Primitive::Number(3));
+    let mut primitives = Vec::new();
+    primitives.push(Primitive::Subtract);
+    primitives.push(Primitive::Number(10));
     primitives.push(Primitive::Number(4));
+    primitives.push(Primitive::Number(5));
     let result = evaluate(primitives);
-    println!("{}", result);
-
-    // HW 8: New main() function that is a new expression
-    // new vector because primitives vector is out of scope
-    let mut primitives_two =  Vec::<Primitive>::new();
-    primitives_two.push(Primitive::Add);
-    primitives_two.push(Primitive::Number(20));
-    primitives_two.push(Primitive::Number(result));
-    let new_result = evaluate(primitives_two);
-    println!("New Result: {}", new_result);
-    
-    // HW 9: Subtraction (25-20=5)
-    let x = 25;
-    let y = 20;
-    let mut sub_primitives =  Vec::<Primitive>::new();
-    sub_primitives.push(Primitive::Subtract);
-    sub_primitives.push(Primitive::Number(x));
-    sub_primitives.push(Primitive::Number(y));
-    let difference = evaluate(sub_primitives);
-    println!("Difference of {} and {} = {}", x, y, difference);
+    println!("result was {}", result);
 }
